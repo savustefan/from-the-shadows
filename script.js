a7e9e0ed6a2967a868aa2939c2847e09fca246e7
Savu Adrian <72447658+savustefan@users.noreply.github.com>
2021-10-11 09:03:48 +0300

    Initial version

---
+++ script.js
@@ -0,0 +1,7 @@
+function Start() {
+    let HeroName = prompt("What is the name of your character?")
+}
+
+function HowToPlay() {
+    let HowToPlay = prompt("Welcome to your adventure! This games plays around prompt() method. When you need to make a decision, write the desired choice in the text box. Enjoy!", "Press Ok to close this window")
+}
