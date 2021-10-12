849fcb562e2b8bf2e6a9630e2718af49e04761c9
Savu Adrian <72447658+savustefan@users.noreply.github.com>
2021-10-12 19:47:01 +0300

    Prologue button functionality

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ function Start() {
 }
 
 function HowToPlay() {
-    let HowToPlay = prompt("Welcome to your adventure! This games plays around prompt() method. When you need to make a decision, write the desired choice in the text box. Enjoy!", "Press Ok to close this window")
+    let HowToPlay = prompt("Welcome to your adventure! This games plays around prompt() method. When you need to make a decision, write the desired choice in the text box. I strongly recommend to listen to the prologue before starting your adventure. Enjoy!", "Press Ok to close this window")
+}
+
+/*Prologue setup*/
+let Audio = document.getElementById("PrologueAudio")
+function PlayAudio() {
+    Audio.play()
 }
