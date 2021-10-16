2938885fc6cfd1f3b7b710766e0276110591b020
Savu Adrian <72447658+savustefan@users.noreply.github.com>
2021-10-16 18:59:58 +0300

    Story content and functionality II
    Further story content and some dialogue reworks.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 /*Buttons Setup*/
 
 function HowToPlay() {
-    let HowToPlay = prompt("Welcome to your adventure! This games plays around prompt() method. When you need to make a decision, write the desired choice in the text box, respectively the letter of the corresponding choice. I strongly recommend to listen to the prologue before starting. Enjoy!", "Press Ok to close this window")
+    let HowToPlay = Swal.fire({
+        title: "'It was the beginning of a very long education...' - Garret, The Thief",
+        imageUrl: 'Trainer.png',
+        imageAlt: 'Tutorial image',
+        imageHeight: 250,
+        text: "Welcome, young adventurer! I will instruct you the various skills you need to know before embarking on your quest.\n" +
+            "This games plays around prompt() method. When you need to make a decision, write the desired choice in the text box, respectively the letter of the corresponding choice.\n" +
+            "Each background and class offers different set of skills and abilities.\n" +
+            "Now you are ready, listen to the prologue. Enjoy!",
+        showClass: {
+            popup: "animate__animated animate__zoomIn"
+        },
+        hideClass: {
+            popup: "animate__animated animate__fadeOutDown"
+        }
+    })
 }
 
 /*Prologue setup*/
@@ -12,8 +27,10 @@ function PlayAudio() {
 
 /*Chapter 1*/
 function Start() {
+
+
     alert("Chapter 1: When light becomes shadow, fear the age of darkness - Pagan prayer")
-    alert("{??} It was about like any other day; rob the rich, don't get caught by the city guard... Business as usual. I was about to go the Clocktower to get some rest when I saw that the entrance's door opened; i had a surprise guest.")
+    alert("{??}: It was about like any other day; rob the rich, don't get caught by the city guard... Business as usual. I was about to go the Clocktower to get some rest when I saw that the entrance's door opened; I had a surprise guest.")
     let Clocktower = prompt("A - To through the door/ B - Find an alternative.")
 
     if (Clocktower === "A" || Clocktower === "B")
@@ -21,51 +38,62 @@ function Start() {
     if (Clocktower === "A") {
         alert("Keeper: I've heard a lot about you. Your skill proceeds your reputation, Master Thief.\n" +
             "{??}: Who are you?\n" +
-            "Keeper Marthus : My name is brother Marthus and we need you help.\n" +
-            "{??}: I accepted right away, I knew there was gonna be lots of coin involved.")
+            "Keeper Marthus: My name is Keeper Marthus and we need you help.\n" +
+            "{??}: I knew something was up, but the instinct told me there will be lot of coins to be made.")
     } else if (Clocktower === "B") {
-        alert("{??} I climbed over the rooftops and got to my hideout. It took a while, but nobody was here. On the table, there was a note\n" +
-            "\t{Note}Master Thief, we need your help. Come to St. John's Cathedral.")
+        alert("{??}: I climbed over the rooftops and got to my hideout. It took a while, but nobody was here. On the table, there was a note\n" +
+            "\t{Note}: Master Thief, we need your help. Come to St. John's Cathedral." +
+            "{??}: Keepers... Something tells me this ")
     }
 
 
-    let HeroName = prompt("I'm glad you came, Master Thief. Or should i call you... (What is the name of your character?)")
+    let HeroName = prompt("Master Thief, you are a sight for the sore eyes. Or should i call you... (What is the name of your character?)")
     let gender = prompt("What is the gender of your character? (Male, Female)")
     let race = prompt("What is your race? (Human, Elf, Tiefling")
 
     /**------------------------------------------**/
-    if (race !== "Human", "Elf", "Tierfling") {
-        prompt(race + "is not accepted into the City. Only Human, Elf and Tiefling")
-    }
 
     let background = prompt("What was your previous job? (Knight, Scholar, Bard)")
     if (gender === "Male") {
         alert("Master " + HeroName + ", we require your services.")
-    } else if (gender === "Female") {
-        alert(HeroName + ". Huh, never thought that the Master Thief will be a woman. Unimportant, we have more pressing matters.")
-    }
+    } else if (gender === "Female" && race === "Elf") {
+        alert("Master " + HeroName + ". Your agility is cunning is unmatched. Never thought you will be an elf. Nevermind, to the matters at hand.")
+    } else if (gender === "Female")
+        alert(HeroName + ". I'm glad you came because we need you help.")
 
     alert("Keeper Marthus: Those damn Pagans managed to steal your Book of Glyphs. We need you to go in their territory and steal it back")
     if (background === "Scholar") {
-        alert("That book contains secrets of the Glyphs, including their location, and how nullifies Pagan magic.")
+        alert("(Scholar Perk - The First Sin): That book contains secrets of the Glyphs, including their location and how nullifies Pagan magic. This is bad...")
     } else if (background === "Knight" || background === "Bard") {
         alert("I suspect a substantial reward.")
     }
 
-    alert("Keeper Marthus: I will take that as a 'Ýes'.")
+    alert("Keeper Marthus: I will take the quest then.")
 
+
+    if (Clocktower === "A" && background === "Bard") {
+        alert("(Ability activated - Toss a coin to your thief) Your man said something earlier in the Clocktower about lot of coins. Guess he was speaking the truth.")
+    }
+
+    alert("Achievement unlocked! 2 Minutes to Midnight - Finish Chapter 1")
     /*Chapter 2*/
 
     alert("Chapter 2\n" +
         "The Hammer is the most blessed of all the Builder's works: ‘Tis both a tool for building, and a weapon against thy foes. - The Hammer Book of Lessons")
-    alert(HeroName + ": Those Keepers said something that Pagans will have special artifacts that I need to destroy to prevent them from using the book's power. Hammer time!")
+    alert(HeroName + ": Those Keepers said something that Pagans will have special artifacts that I need to destroy to prevent them from using the book's power. It's Hammer time!")
+    alert("*You arrived at the Hammerite's Cathedal*\n" +
+
+           HeroName +": Here we are. That 'holy' thing must be somewhere in there. Hmm...")
     if (race === "Elf" && background === "Bard") {
         alert("(Ability activated - Whisper Ways) *footsteps*\n" +
+
             "{Hammerite Guard 1}: Shall we gather for whisky and cigars tonight?\n" +
             "{Hammerite Guard 2}: Are you kidding? We need to keep watch on The Builder's Hammer. Those Pagans may attack anytime.\n" +
             "{Hammerite Guard 1}: Oh, yeah. They moved it to the Master Forger's Quarters.\n" +
             "{Hammerite Guard 2}: SHHHHHHH, IDIOT! You don't know who could listen from the shadows.")
+        alert(HeroName + ": Only 1 guard at the entrance. This should be pretty easy. A - Sneak past the guard and go inside/ B - Knock him out with your trusty Blackjack")
     } else if (race === "Human", "Tierfling"){
-        alert()
+        alert(HeroName + ": Only 1 guard at the entrance. This should be pretty easy. A - Sneak past the guard and go inside/ B - Persuade the guard to tell you about the Hammer's location")
     }
+    
 }
